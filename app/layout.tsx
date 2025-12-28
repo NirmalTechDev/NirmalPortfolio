cb332dc79fe7334cee00b03f60ab73e3b4169810
Nirmal Patel <cdmi.nirmal.2211@gmail.com>
2025-12-28 20:09:15 +0530

    again update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import React from "react";
@@ -17,10 +17,13 @@ export const metadata: Metadata = {
     title: "Nirmal Ranpariya ✦ Full-Stack Software Developer Portfolio",
     description:
         "Nirmal Ranpariya | Full-Stack Software Developer (React Native, React.js, Node.js). 2+ years of crafting high-performance, scalable applications.",
-    themeColor: "#0ea5e9",
     verification: {
         google: "Pxd07Sb1Z_mmF3Th9wMF_zVPGuMjA7R_1ecg7KV0sf0",
     },
+};
+
+export const viewport: Viewport = {
+    themeColor: "#0ea5e9",
 };
 
 export default function RootLayout({
